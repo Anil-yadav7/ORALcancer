c94e83dbfc09e6fc61aa5a3de706593bc66d9680
Anil Kumar <anilkumar@Anils-MacBook-Air.local>
2026-07-30 23:30:16 +0530

    again

--- src/gan_model.py
+++ src/gan_model.py
@@ -9,10 +9,10 @@ class Generator(nn.Module):
     def __init__(self, noise_dim=128, num_classes=5, features_g=64):
         super(Generator, self).__init__()        
         self.features_g = features_g
+        # Label embedding maps class labels to one-hot embedding vectors matching num_classes
         self.label_emb = nn.Embedding(num_classes, num_classes)
         
         self.init_proj = nn.Sequential(
-            # bias=False because it is followed by BatchNorm
             nn.Linear(noise_dim + num_classes, features_g * 16 * 8 * 8, bias=False),
             nn.BatchNorm1d(features_g * 16 * 8 * 8),
             nn.ReLU(True)
@@ -40,7 +40,6 @@ class Generator(nn.Module):
             nn.ReLU(True),
             
             # Block 5 (128x128 -> 256x256)
-            # bias=True here because there is no BatchNorm after it
             nn.ConvTranspose2d(features_g, 3, kernel_size=4, stride=2, padding=1, bias=True),
             nn.Tanh() 
         )
@@ -61,15 +60,12 @@ class Critic(nn.Module):
     def __init__(self, features_d=64, num_classes=5):
         super(Critic, self).__init__()
         
-        # Memory Optimized: Embed to a single channel rather than 256*256 pixels
         self.label_emb = nn.Embedding(num_classes, 1)
         
         self.model = nn.Sequential(
-            # Input: 4 channels (3 RGB + 1 Label Map)
             nn.Conv2d(4, features_d, kernel_size=4, stride=2, padding=1, bias=True),
             nn.LeakyReLU(0.2, inplace=True),
             
-            # GroupNorm(1, C) is mathematically identical to LayerNorm but adapts to any spatial size
             nn.Conv2d(features_d, features_d * 2, kernel_size=4, stride=2, padding=1, bias=False),
             nn.GroupNorm(1, features_d * 2), 
             nn.LeakyReLU(0.2, inplace=True),
@@ -91,7 +87,6 @@ class Critic(nn.Module):
         )
 
     def forward(self, img, labels):
-        # Broadcast the 1D embedding to match the image's spatial dimensions (H, W)
         c = self.label_emb(labels).view(-1, 1, 1, 1)
         c = c.expand(-1, -1, img.size(2), img.size(3))
         
